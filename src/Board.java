76a6f67b548fd57c42255c0d26c7182f7fb04ceb
chrisfishbob <chrisfishbob@gmail.com>
2021-07-08 13:28:29 -0700

    moveGenerator class created for refactoring move generation out of the main Board class

--- src/Board.java
+++ src/Board.java
@@ -51,7 +51,7 @@ public class Board extends PApplet {
         loadImages();
         board = new Piece[64];
         loadFromFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
-        potentialLegalKnightMoveMap = preGenerateKnightMoves();
+        potentialLegalKnightMoveMap = MoveGenerator.preGenerateKnightMoves();
     }
 
 
@@ -508,43 +508,6 @@ public class Board extends PApplet {
         if (verificationSuccessful){
             System.out.println("Board verification successful");
         }
-    }
-
-
-    public static HashMap<Integer, ArrayList<Move>> preGenerateKnightMoves(){
-        // Pre-generates all the potential legal knight moves during set up.
-        // Moves that turn out to be illegal during run time are filtered out
-        // in the generateMoves method for the Knight object.
-
-        HashMap<Integer, ArrayList<Move>> potentialLegalKnightMovesMap = new HashMap<>();
-        int targetSquare;
-        int[][] knightOffsets = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, - 2}, {1, 2}, {2, -1}, {2, 1}};
-
-        // Loops through all the squares and calculates all the potentially legal knight moves
-        for (int boardIndex = 0; boardIndex < 64; boardIndex++){
-            int rank = boardIndex / 8;
-            int file = boardIndex % 8;
-            ArrayList<Move> potentialLegalKnightMoves = new ArrayList<>();
-
-            // A perfectly placed knight has eight legal moves. Loop through all of them and check
-            // if they are potentially legal (does not move out of bounds)
-            for (int i = 0; i < 8; i ++){
-                if (isInBounds(rank + knightOffsets[i][0], file + knightOffsets[i][1])){
-                    targetSquare = (rank + knightOffsets[i][0]) * 8 + (file + knightOffsets[i][1]);
-                    potentialLegalKnightMoves.add(new Move(boardIndex, targetSquare));
-                }
-            }
-
-            potentialLegalKnightMovesMap.put(boardIndex, potentialLegalKnightMoves);
-        }
-
-        return potentialLegalKnightMovesMap;
-    }
-
-
-
-    public static boolean isInBounds(int rank, int file){
-        return rank >= 0 && rank < 8 && file >= 0 && file < 8;
     }
 
     public static Piece[] getBoard(){
---
+++ src/MoveGenerator.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MoveGenerator {
+    public static HashMap<Integer, ArrayList<Move>> preGenerateKnightMoves(){
+        // Pre-generates all the potential legal knight moves during set up.
+        // Moves that turn out to be illegal during run time are filtered out
+        // in the generateMoves method for the Knight object.
+
+        HashMap<Integer, ArrayList<Move>> potentialLegalKnightMovesMap = new HashMap<>();
+        int targetSquare;
+        int[][] knightOffsets = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, - 2}, {1, 2}, {2, -1}, {2, 1}};
+
+        // Loops through all the squares and calculates all the potentially legal knight moves
+        for (int boardIndex = 0; boardIndex < 64; boardIndex++){
+            int rank = boardIndex / 8;
+            int file = boardIndex % 8;
+            ArrayList<Move> potentialLegalKnightMoves = new ArrayList<>();
+
+            // A perfectly placed knight has eight legal moves. Loop through all of them and check
+            // if they are potentially legal (does not move out of bounds)
+            for (int i = 0; i < 8; i ++){
+                if (isInBounds(rank + knightOffsets[i][0], file + knightOffsets[i][1])){
+                    targetSquare = (rank + knightOffsets[i][0]) * 8 + (file + knightOffsets[i][1]);
+                    potentialLegalKnightMoves.add(new Move(boardIndex, targetSquare));
+                }
+            }
+
+            potentialLegalKnightMovesMap.put(boardIndex, potentialLegalKnightMoves);
+        }
+
+        return potentialLegalKnightMovesMap;
+    }
+
+    public static boolean isInBounds(int rank, int file){
+        return rank >= 0 && rank < 8 && file >= 0 && file < 8;
+    }
+}
